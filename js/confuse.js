b6d1dfeb587099fd2c5e6278377716d421f25270
Chris Weber <chris@lookout.net>
2013-10-13 10:27:29 -0700

    Whoops

--- js/confuse.js
+++ js/confuse.js
@@ -1,8 +1,26 @@
 function confuse(input) {
-	var output;
+	var output = "";
 	if (typeof input === "string") {
 		// Input is a valid string
-        output = Confusables[input][(Math.round(Math.random() * (Confusables[input].length - 1)))];
+
+		// Iterate over string
+		for (var i = 0; i < input.length; i++) {
+			// Get the current character
+			var c = input.charAt(i);
+			// Flip a coin to decide whether or not this 
+			// character gets replaced with a confusable
+			var chance = parseInt(Math.random() * 2, 10);
+			// Get the value of the character
+			var v = c.charCodeAt();
+
+			// Only confuse things if the char is in the BMP
+			if (chance && v <= 0xFFFF) {
+        		output = output + String.fromCodePoint(Confusables[v][(Math.round(Math.random() * (Confusables[v].length - 1)))]);
+			}
+			else {
+				output = output + c;
+			}
+		}
 		// For each charac
 	}
 	return output;
