0082ad48df12432d419b96154278802778151e63
Chris Weber <chris@lookout.net>
2013-12-22 15:18:59 -0800

    Adding analytics

---
+++ js/analytics.js
@@ -0,0 +1,21 @@
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-1741389-19']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+function trackButtonClick(e) {
+    _gaq.push(['_trackEvent', e.target.id, 'clicked']);
+};
+
+// Track which features are getting used
+document.addEventListener('DOMContentLoaded', function () {
+  var buttons = document.querySelectorAll('button');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', trackButtonClick);
+  }
+});
